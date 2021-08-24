d5f6a82f96b25ca3469eb5a81b5beb92b536b112
jerrx18 <happyjerrychristmas@gmail.com>
2021-08-24 18:05:31 +0900

    day1 char

--- character.py
+++ character.py
@@ -9,3 +9,27 @@ print(chr(72))
 
 print(ord('제'))
 print(ord('영'))
+
+
+print("HELLO, WORLD")
+print('HELLO, WORLD')
+
+# print('HELLO") ' " 맞춰서 쓰기 / 인식불가
+
+
+# +로 문자열 연결
+print("HELLO" + 'world')
+print("HELLO " + "WORLD")
+print("hello" + " world")
+print("hello" + " " + "world")
+
+#빈 문자열
+print("hello" + "" + "world")
+
+#문자열 반복
+print("jerry" * 3)
+print("jerry" * 0)
+
+#jerry's birthday
+print("jerry's birthday")
+print('jerry\'s birthday')
